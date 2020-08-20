83ed1dfadf579c851f7be3fd1746c1839a8f2ad3
your name here <skypinacolada@gmail.com>
2020-08-20 18:06:51 +0900

    Solve target_number

--- BFS_DFS/target_number.py
+++ BFS_DFS/target_number.py
@@ -1,15 +1,13 @@
-from itertools import combinations 
+from itertools import combinations
+
 
 def solution(numbers, target):
     answer = 0
-    mini = (-1)*sum(numbers)
     maxi = sum(numbers)
-    if target == mini or target == maxi : return 1
-    if target < mini or target > maxi : return 0 
     minus = maxi - target
     for i in range(1, len(numbers)):
-        combs = list(map(lambda x : x, combinations(numbers,i)))
-        for comb in combs : 
-            if sum(comb)*2 == minus: answer += 1
+        combs = list(map(lambda x: x, combinations(numbers, i)))
+        for comb in combs:
+            if sum(comb)*2 == minus:
+                answer += 1
     return answer
-
